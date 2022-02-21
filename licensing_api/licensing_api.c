79ce3ac28c81d387407664053fb1107924238408
jondy <jondyowens@gmail.com>
2022-02-21 06:38:52 -0800

    changed to be a dynamic library instead of static. generate_hardware_hash function now returns hash instead of printing

--- licensing_api/licensing_api.c
+++ licensing_api/licensing_api.c
@@ -3,18 +3,14 @@
 
 #include "pch.h"
 #include "licensing_api.h"
-#include <Windows.h>
 #include <stdio.h>
 // TODO: This is an example of a library function
-void generate_hardware_hash()
+const WCHAR* generate_hardware_hash()
 {
     HW_PROFILE_INFO hwProfileInfo;
     
     if (GetCurrentHwProfile(&hwProfileInfo) != NULL)
     {
-        printf("Hardware GUIID: %ls\n", hwProfileInfo.szHwProfileGuid);
-        printf("Hardware Profile: %ls\n", hwProfileInfo.szHwProfileName);
+        return hwProfileInfo.szHwProfileGuid;
     }
-
-    getchar();
 }
--- licensing_api/licensing_api.h
+++ licensing_api/licensing_api.h
@@ -1,1 +1,10 @@
-void generate_hardware_hash();
+#pragma once
+#include <Windows.h>
+
+#ifdef licensing_api_exports
+#define licensing_api __declspec(dllexport)
+#else
+#define licensing_api __declspec(dllimport)
+#endif
+
+licensing_api const WCHAR* generate_hardware_hash();
